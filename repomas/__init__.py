1caeceb97a05a75d74c5f3340560804a2d558d7f
oumao <evansotis2015@gmail.com>
2021-03-06 12:58:27 +0300

    Configuring Migrate and Registering Blueprints

--- repomas/__init__.py
+++ repomas/__init__.py
@@ -1,21 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask
+from flask_login import LoginManager
+from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
 
 db = SQLAlchemy()
+migrate = Migrate()
+login_manager = LoginManager()
 
 def create_app(config):
-    
+
     app = Flask(__name__, instance_relative_config=True)
-    app.config.from_object[config]
-    app.config.from_pyfile(config.py)
+    app.config.from_object('config')
+    app.config.from_pyfile('config.py')
 
 
     db.init_app(app)
-
-# @app.route('/')
-# def home():
-#     return render_template("home.html", title="Home")
+    migrate.init_app(app, db)
+    login_manager.init_app(app)
 
 
-# app.run(debug=True)
+    from repomas import models
+
+    from .admin import admin as admin_blueprint
+    app.register_blueprint(admin_blueprint)
+
+    from .student import student as student_blueprint
+    app.register_blueprint(student_blueprint)
+
+
+    return app
