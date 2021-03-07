33cf4919292efbc79f5a71a817b8c62a8edba838
oumao <evansotis2015@gmail.com>
2021-03-07 12:38:43 +0300

    Created CRUD template for student

--- repomas/student/views.py
+++ repomas/student/views.py
@@ -4,3 +4,16 @@ from . import student
 @student.route('/student/registration', methods=['POST', 'GET'])
 def registration():
     return render_template('student/registration', title='Student Registration')
+
+@student.route('/students/', methods=['POST', 'GET'])
+def all_students():
+    return render_template('student/students.html', title='Students')
+
+
+@student.route('/student/<int:id>', methods=['POST', 'GET'])
+def one_student(id):
+    return render_template('student/student.html', title='Student')
+
+@student.route('/students/<int:id>', methods=['POST', 'GET'])
+def update_student(id):
+    return render_template('student/update.html', title='Student Update')
