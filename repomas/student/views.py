fb548a225d7284c829b201a15e4a17d5cbcf9830
oumao <evansotis2015@gmail.com>
2021-03-08 00:06:49 +0300

    Finished the CRUD for student module

--- repomas/student/views.py
+++ repomas/student/views.py
@@ -1,19 +1,90 @@
-from flask import render_template
+from datetime import datetime as dt
+from flask import flash, url_for, abort, request
+from flask_login import login_required
+from repomas.student.forms import StudentRegistrationForm, StudentUpdateForm
+from flask import render_template, redirect
+from repomas import db
+from repomas.models import Student
 from . import student
 
 @student.route('/student/registration', methods=['POST', 'GET'])
-def registration():
-    return render_template('student/registration', title='Student Registration')
+@login_required
+def add_student():
+    form = StudentRegistrationForm()
+    if form.validate_on_submit():
 
-@student.route('/students/', methods=['POST', 'GET'])
+        student = Student(first_name=form.first_name.data, last_name=form.last_name.data, 
+                    admission=form.admission.data, birthdate=form.birthdate.data, zipcode=form.zipcode.data,
+                    county=form.county.data, ward=form.ward.data)
+        db.session.add(student)
+        db.session.commit()
+        flash("Successfully Added a student", "success")
+        return redirect(url_for('admin.dashboard'))
+
+    return render_template('student/register.html', form=form, title='Student Registration')
+
+@student.route('/student/all', methods=['POST', 'GET'])
 def all_students():
-    return render_template('student/students.html', title='Students')
+    students = Student.query.all()
+    return render_template('student/students_table.html', title='Students', students=students)
 
 
-@student.route('/student/<int:id>', methods=['POST', 'GET'])
-def one_student(id):
-    return render_template('student/student.html', title='Student')
+@student.route('/student/<int:student_id>', methods=['POST', 'GET'])
+@login_required
+def student_detail(student_id):
+    student = Student.query.get_or_404(student_id)
+    currentyr = dt.utcnow()
+    print(currentyr)
+    return render_template('student/student_details.html', title='Student', student=student, year=currentyr)
 
-@student.route('/students/<int:id>', methods=['POST', 'GET'])
-def update_student(id):
-    return render_template('student/update.html', title='Student Update')
+@student.route('/students/<int:student_id>/update/', methods=['POST', 'GET'])
+@login_required
+def update_student(student_id):
+
+    student = Student.query.get_or_404(student_id)
+
+    if not student:
+        abort(403)
+
+    form = StudentUpdateForm()
+
+    if form.validate_on_submit():
+
+        student.first_name = form.first_name.data
+        student.last_name = form.last_name.data
+        student.admission = form.admission.data
+        student.birthdate = form.birthdate.data
+        student.zipcode = form.zipcode.data
+        student.county = form.county.data
+        student.ward = form.ward.data
+
+        db.session.commit()
+
+        flash("Successfully Updated the student", "success")
+        return redirect(url_for('student.all_students'))
+
+    elif request.method == 'GET':
+
+        form.first_name.data = student.first_name
+        form.last_name.data = student.last_name
+        form.admission.data = student.admission
+        form.birthdate.data = student.birthdate
+        form.zipcode.data = student.zipcode
+        form.county.data = student.county
+        form.ward.data = student.ward
+
+    return render_template('student/register.html', title='Student Update', 
+                                    legend="Update Student Details", form=form)
+
+@student.route('/student/<int:student_id>/delete/', methods=['POST', 'GET'])
+@login_required
+def delete_student(student_id):
+    student = Student.query.get_or_404(student_id)
+    if student:
+        db.session.delete(student)
+        db.session.commit()
+        flash("Successfully deleted the student from the database", "success")
+        return redirect(url_for('student.all_students'))
+    else:
+        flash("The student does not exist", "warning")
+        return redirect(url_for('student.all_students')) 
