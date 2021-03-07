91b2bbfd9f9d1dda51834c9c48a787e4ffce5df4
oumao <evansotis2015@gmail.com>
2021-03-07 23:59:56 +0300

    Added Medical Blueprint

--- repomas/__init__.py
+++ repomas/__init__.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from flask_login import LoginManager
 from flask_migrate import Migrate
@@ -10,14 +11,26 @@ login_manager = LoginManager()
 
 def create_app(config):
 
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_object('config')
-    app.config.from_pyfile('config.py')
+    if os.getenv('FLASK_CONFIG') == "production":
+        app = Flask(__name__)
+        app.config.update(
+            SECRET_KEY=os.getenv('SECRET_KEY'),
+            SQLALCHEMY_DATABASE_URI=os.getenv('SQLALCHEMY_DATABASE_URI')
+        )
+    else:
+        app = Flask(__name__, instance_relative_config=True)
+        app.config.from_object('config')
+        app.config.from_pyfile('config.py')
 
 
     db.init_app(app)
     migrate.init_app(app, db)
     login_manager.init_app(app)
+
+    login_manager.login_message = "You must be logged in to access this page."
+    login_manager.login_view = "admin.login"
+    login_manager.login_message_category = "info"
+
 
 
     from repomas import models
@@ -28,6 +41,8 @@ def create_app(config):
     from .student import student as student_blueprint
     app.register_blueprint(student_blueprint)
 
+    from .medical import med as medical_blueprint
+    app.register_blueprint(medical_blueprint)
 
 
     return app
