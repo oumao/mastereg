554ac3b2382896d6af5c84269828a95c2a5b2456
oumao <evansotis2015@gmail.com>
2021-03-13 02:39:29 +0300

    Removed the commented Medical display html

--- repomas/medical/forms.py
+++ repomas/medical/forms.py
@@ -33,22 +33,3 @@ class MedicalStatusUpdateForm(FlaskForm):
 
     submit = SubmitField('Update Records')
 
-
-
-
-#   <!-- <div class="card-body">
-#             <h3 class="card-title">Medical History</h3>
-#             <div class="row">
-#                 <p class="col-4">Height {{ }}</p>
-#                 <p class="col-4">Weight: {{ }}</p>
-#                 <p class="col-4">Disability: {{ }}</p>
-#             </div>
-#             <div class="row">
-#                 <p class="col-8">Diagnosis: {{ }}</p>
-#                 <p class="col-4">Underlying Condition: {{ }}</p>
-#             </div>
-#             <div class="row">
-#                 <p class="col-3">Drug Prescribed: {{ }} </p>
-#                 <p class="col-3">Outcome: {{ }}</p>
-#             </div>
-#         </div> -->
