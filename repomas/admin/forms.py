6f272afefc2be7f97b75c53e00fc044ede6e2c16
oumao <evansotis2015@gmail.com>
2022-06-11 23:53:49 +0300

    Modified Admin Dashboard

--- repomas/admin/forms.py
+++ repomas/admin/forms.py
@@ -2,46 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField
 from wtforms.validators import DataRequired, EqualTo, Email, Length, ValidationError
 
-from repomas.models import Admin
+from repomas.models import User
 
-
-
-class AdminRegistrationForm(FlaskForm):
-    
-    first_name = StringField('First Name', validators=[DataRequired()])
-    last_name = StringField('Last Name', validators=[DataRequired()])
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired(), Length(min=6, max=20)])
-    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(),
-                            EqualTo('password')])
-
-    submit = SubmitField('Register')
-
-    def validate_email(self, email):
-        admin = Admin.query.filter_by(email=email.data).first()
-        if admin:
-            raise ValidationError('That Email is taken. Please choose a different one.')
-
-    def validate_username(self, username):
-        admin = Admin.query.filter_by(username=username.data).first()
-        if admin:
-            raise ValidationError('That username is taken. Please choose a different one.')
-
-
-class AdminLoginForm(FlaskForm):
-    
-    username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired(), Length(min=6, max=20)])
-
-    submit = SubmitField('Login')
-
-
-class AdminUpdationForm(FlaskForm):
-    
-    first_name = StringField('First Name', validators=[DataRequired()])
-    last_name = StringField('Last Name', validators=[DataRequired()])
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    username = StringField('Username', validators=[DataRequired()])
-
-    submit = SubmitField('Update Profile ')
+class UpdateForm(FlaskForm):
+    pass
--- repomas/admin/views.py
+++ repomas/admin/views.py
@@ -1,70 +1,12 @@
-from flask import render_template, redirect, url_for, flash
-from flask_login import login_user, logout_user, login_required, current_user
-from werkzeug.security import generate_password_hash, check_password_hash
-from .forms import AdminRegistrationForm, AdminLoginForm
-
+from flask import render_template
+from flask_login import login_required, current_user
 from repomas import db
-from repomas.models import Admin
+from repomas.models import User
 from . import admin
-    
-
-@admin.route('/admin/registration', methods=['POST', 'GET'])
-def registration():
-
-    admin = Admin.query.filter_by(id=1).first()
-
-    if admin:
-        return redirect(url_for('admin.login'))
-    else:
-
-        form = AdminRegistrationForm()
-
-        if form.validate_on_submit():
-
-            hashed_pass = generate_password_hash(form.password.data)
-            new_admin = Admin(first_name=form.first_name.data, last_name=form.last_name.data,
-                        email=form.email.data, username=form.username.data, password=hashed_pass)
-
-            db.session.add(new_admin)
-            db.session.commit()
-            
-            flash("Administrator Successfully Added", "success")
-            return redirect(url_for('admin.login'))
-            
-        return render_template('admin/registration.html', form=form)
-
-    
-
-@admin.route('/admin/login', methods=['POST', 'GET'])
-def login():
-    
-    form = AdminLoginForm()
-    if form.validate_on_submit():
-
-        admin = Admin.query.filter_by(username=form.username.data).first()
-
-        if admin and check_password_hash(admin.password, form.password.data):
-            login_user(admin)
-            flash("Login Succes", "success")
-            return redirect(url_for('admin.dashboard'))
-        else:
-            flash("There is no administrator with such credentials!!. Please check your details and try again", "danger")
-            return redirect(url_for('admin.login'))
-
-    return render_template('admin/login.html', form=form)
-   
 
 
-
-@admin.route('/admin/dashboard', methods=['GET', 'POST'])
+@admin.route('/dashboard', methods=['GET', 'POST'])
 @login_required
 def dashboard():
     return render_template("admin/dashboard.html", title="Admin Dashboard")
 
-
-@admin.route('/admin/logout', methods=['GET', 'POST'])
-@login_required
-def logout():
-    logout_user()
-    flash('You have successfully Logged Out','success')
-    return redirect(url_for('admin.login'))
